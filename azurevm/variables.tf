67eaf453803dee63667cc2f313b4e01499c1dfe5
chianw <chiancw@gmail.com>
2023-01-09 15:19:12 +0800

    first project

---
+++ azurevm/variables.tf
@@ -0,0 +1,17 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  default     = "tftest"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default     = "East US"
+}
+
+variable "username" {
+  description = "The username to log in"
+}
+
+variable "password" {
+  description = "The password to log in"
+}
